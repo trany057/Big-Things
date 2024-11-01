33741c067d4e9ca91a7aebc4c941f85a6161717d
kim.tran2610@gmail.com <kim.tran2610@gmail.com>
2024-11-01 21:11:21 +0700

    Initial Commit

---
+++ Big Things/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Big Things
+//
+//  Created by An Bảo on 1/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
